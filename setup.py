e7cb497e26da3ef3d200f04f211513c7a83ddc9c
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-03-01 12:29:42 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='bbox_utils',
+    version='0.1.0',
+    url="https://https://github.com/amedyukhina/bbox_detection",
+    author="Anna Medyukhina",
+    author_email='anna.medyukhina@gmail.com',
+    packages=['bbox_utils'],
+    license='Apache License Version 2.0',
+    include_package_data=True,
+
+    test_suite='tests',
+
+    install_requires=[
+    ],
+)
