a38793a3d21bc43160b3cee5d92cea621c84950c
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-03-02 12:31:49 -0600

    updated name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='bbox_utils',
+    name='ml_utils',
     version='0.1.0',
     url="https://https://github.com/amedyukhina/bbox_detection",
     author="Anna Medyukhina",
