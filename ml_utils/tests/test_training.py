f80ae82454d01d54f7cf741da9207ffad503c17a
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-03-02 15:38:35 -0600

    plot gt and predicted bboxes

--- ml_utils/tests/test_training.py
+++ ml_utils/tests/test_training.py
@@ -4,6 +4,7 @@ import time
 import unittest
 
 import numpy as np
+import pandas as pd
 import wandb
 from ddt import ddt, data
 from skimage import io
@@ -12,6 +13,7 @@ from ..model.faster_rcnn import load_model_for_training
 from ..predict.predict_bbox import detect_bboxes
 from ..train.dataloader_bbox import get_data_loaders
 from ..train.train_bbox import train
+from ..utils.utils import join_bboxes
 from ..utils.visualize import draw_bbox, overlay_bboxes_batch
 
 INPUT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/../../example_data'
@@ -45,7 +47,7 @@ class TestTraining(unittest.TestCase):
                                          input_dir=os.path.join(INPUT_DIR, 'img'),
                                          val_fraction=0.2, batch_size=2, num_workers=2)
         model = load_model_for_training()
-        config = dict(num_epochs=2, lr=0.01, momentum=0.9, weight_decay=0.0005,
+        config = dict(num_epochs=5, lr=0.01, momentum=0.9, weight_decay=0.0005,
                       step_size=3, gamma=0.1)
         if log_progress is False:
             os.environ['WANDB_MODE'] = 'offline'
@@ -63,12 +65,18 @@ class TestTraining(unittest.TestCase):
                            batch_size=2, overlap_threshold=0.1, detection_threshold=0.1)
         self.assertGreater(len(df), 0)
 
+        df_gt = pd.read_csv(os.path.join(INPUT_DIR, 'bboxes.csv'))
+        df = join_bboxes(df_gt, df, cl_name='class')
+        print(df)
+
         overlay_bboxes_batch(df=df, input_dir=os.path.join(INPUT_DIR, 'img'),
-                             output_dir=INPUT_DIR + rf'/../tmp/overlay/{model_name}')
+                             output_dir=INPUT_DIR + rf'/../tmp/overlay/{model_name}',
+                             palette=[(0, 255, 0), (255, 0, 0)])
 
         self.assertTrue(os.path.exists(INPUT_DIR +
                                        rf'/../tmp/model/{model_name}/weights_best.pth'))
         self.assertEqual(len(os.listdir(INPUT_DIR + rf'/../tmp/overlay/{model_name}')), 5)
+
         shutil.rmtree(INPUT_DIR + '/../tmp')
 
 
