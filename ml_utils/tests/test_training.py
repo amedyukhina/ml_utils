e271bd4282b2c47eb1645bf87fbfc49c79107d6d
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-03-16 14:43:20 -0500

    changed dataloader to load train and validation sets from separate files

--- ml_utils/tests/test_training.py
+++ ml_utils/tests/test_training.py
@@ -3,18 +3,16 @@ import shutil
 import time
 import unittest
 
-import numpy as np
 import pandas as pd
 import wandb
 from ddt import ddt, data
-from skimage import io
 
 from ..model.faster_rcnn import load_model_for_training
 from ..predict.predict_bbox import detect_bboxes
 from ..train.dataloader_bbox import get_data_loaders
 from ..train.train_bbox import train
 from ..utils.utils import join_bboxes
-from ..utils.visualize import draw_bbox, overlay_bboxes_batch
+from ..utils.visualize import overlay_bboxes_batch
 
 INPUT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/../../example_data'
 
@@ -22,22 +20,21 @@ INPUT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/../../example_data'
 @ddt
 class TestTraining(unittest.TestCase):
 
-    def test_input_data(self):
+    @data(
+        (os.path.join(INPUT_DIR, 'bboxes.csv'), 5), (None, 1)
+    )
+    def test_input_data(self, case):
+        bbox_fn_val, nimg = case
         tr_dl, val_dl = get_data_loaders(os.path.join(INPUT_DIR, 'bboxes.csv'),
+                                         bbox_fn_val=bbox_fn_val,
                                          input_dir=os.path.join(INPUT_DIR, 'img'),
-                                         val_fraction=0.2, batch_size=2, num_workers=2)
+                                         val_fraction=0.2,
+                                         batch_size=2, num_workers=2)
 
-        for images, targets, image_ids in tr_dl:
-            for i in range(len(images)):
-                boxes = targets[i]['boxes'].cpu().numpy().astype(np.int32)
-                sample = images[i].permute(1, 2, 0).cpu().numpy()
-
-                for box in boxes:
-                    sample = draw_bbox(sample, [box[1], box[0], box[3], box[2]], color=(1, 0, 0))
-
-                io.imshow(sample)
-                # import pylab as plt
-                # plt.show()
+        n = 0
+        for images, targets, image_ids in val_dl:
+            n += len(images)
+        self.assertEqual(n, nimg)
 
     @data(
         False
--- ml_utils/train/dataloader_bbox.py
+++ ml_utils/train/dataloader_bbox.py
@@ -8,24 +8,30 @@ from ..utils.convert import wh_to_xy
 from ..utils.utils import collate_fn
 
 
-def get_data_loaders(bbox_fn, input_dir, val_fraction=0.2, seed=None,
-                     id_name='image_id', shuffle=True, **kwargs):
-    df = pd.read_csv(bbox_fn)
-    df = wh_to_xy(df)
+def split_train_val(df, id_name='image_id', val_fraction=0.2, seed=None):
     image_ids = df[id_name].unique()
     nvalid = int(round(val_fraction * len(image_ids)))
     np.random.seed(seed)
     image_ids = np.random.choice(image_ids, len(image_ids), replace=False)
 
     print(rf"Train images: {len(image_ids) - nvalid}; validation images: {nvalid}")
-    valid_ids = image_ids[-nvalid:]
-    train_ids = image_ids[:-nvalid]
+    valid_df = df[df[id_name].isin(image_ids[-nvalid:])]
+    train_df = df[df[id_name].isin(image_ids[:-nvalid])]
+    return train_df, valid_df
+
+
+def get_data_loaders(bbox_fn, input_dir, bbox_fn_val=None, val_fraction=0.2, seed=None,
+                     id_name='image_id', shuffle=True, **kwargs):
+    dfs = [wh_to_xy(pd.read_csv(fn)) for fn in [bbox_fn, bbox_fn_val] if fn is not None]
+    if len(dfs) == 2:
+        train_df, valid_df = dfs
+    else:
+        train_df, valid_df = split_train_val(dfs[0], id_name, val_fraction, seed)
 
     dls = []
-    for ids, shf, tr in zip([train_ids, valid_ids],
-                            [shuffle, False],
-                            [get_train_transform(), get_valid_transform()]):
-        cur_df = df[df[id_name].isin(ids)]
+    for cur_df, shf, tr in zip([train_df, valid_df],
+                               [shuffle, False],
+                               [get_train_transform(), get_valid_transform()]):
         ds = DatasetObjectTraining(cur_df, input_dir, tr)
         dls.append(DataLoader(ds, shuffle=shf, collate_fn=collate_fn, **kwargs))
     return dls
