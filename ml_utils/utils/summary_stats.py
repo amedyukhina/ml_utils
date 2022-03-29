8e72e4e8a2849d414c8df46254df4b7ff3f900f0
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-03-29 12:43:41 -0500

    accuracy calculation

--- ml_utils/utils/summary_stats.py
+++ ml_utils/utils/summary_stats.py
@@ -1,3 +1,9 @@
+import numpy as np
+import pandas as pd
+import torch
+import torchvision.ops.boxes as bops
+
+
 class Averager:
     def __init__(self):
         self.current_total = 0.0
@@ -17,3 +23,42 @@ class Averager:
     def reset(self):
         self.current_total = 0.0
         self.iterations = 0.0
+
+
+def __get_scores(bboxes, gt_boxes):
+    jaccard = np.zeros([len(gt_boxes), len(bboxes)])
+    dist = np.zeros([len(gt_boxes), len(bboxes)])
+    for i in range(len(gt_boxes)):
+        for j in range(len(bboxes)):
+            jaccard[i, j] = bops.box_iou(torch.tensor([gt_boxes[i]], dtype=torch.float),
+                                         torch.tensor([bboxes[j]], dtype=torch.float)).data.cpu().numpy()[0, 0]
+            centers = np.array([[(box[0] + box[2]) / 2, (box[1] + box[3]) / 2] for box in [bboxes[j], gt_boxes[i]]])
+            dist[i, j] = np.sqrt(np.sum((centers[0] - centers[1]) ** 2))
+    return jaccard, dist
+
+
+def __get_match_indices(dist, dist_thr):
+    dist2 = dist.copy()
+    maxval = 10 ** 10
+    inds = []
+
+    for i in range(min(len(dist), len(dist[0]))):
+        ind = np.unravel_index(np.argmin(dist2, axis=None), dist2.shape)
+        if dist[ind] <= dist_thr:
+            inds.append(ind)
+        else:
+            break
+        dist2[ind[0], :] = maxval
+        dist2[:, ind[1]] = maxval
+    return tuple(np.array(inds).transpose())
+
+
+def accuracy(bboxes, gt_boxes, image_id, dist_thr):
+    jaccard, dist = __get_scores(bboxes, gt_boxes)
+    inds = __get_match_indices(dist, dist_thr)
+
+    stats = {'image_id': image_id, 'n ground truth': len(gt_boxes), 'n detected': len(bboxes),
+             'false negatives': len(gt_boxes) - len(dist), 'false positives': len(bboxes) - len(dist),
+             'true positives': len(dist), 'distance error pix': np.mean(dist[inds]),
+             'Jaccard index': np.mean(jaccard[inds])}
+    return pd.Series(stats).to_frame().transpose()
