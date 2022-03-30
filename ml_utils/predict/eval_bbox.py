7669dfd61438cd3a32a69d36a3d22f57e7d857f3
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-03-30 10:54:27 -0500

    removed bbox detection from evaluation code

--- ml_utils/predict/eval_bbox.py
+++ ml_utils/predict/eval_bbox.py
@@ -1,26 +1,19 @@
 import numpy as np
 import pandas as pd
 
-from ..predict.predict_bbox import detect_bboxes
 from ..utils.convert import wh_to_xy
 from ..utils.summary_stats import accuracy, summarize_accuracy
 
 
-def evaluate_accuracy(df_gt, model_fn, input_dir, config, return_full=False):
+def evaluate_accuracy(df, df_gt, dist_thr, return_full=False):
     df_gt = wh_to_xy(df_gt)
-
-    df = detect_bboxes(input_dir=input_dir,
-                       model_fn=model_fn,
-                       batch_size=config.batch_size,
-                       overlap_threshold=config.overlap_thr,
-                       detection_threshold=config.detection_thr)
 
     cols = ['x1', 'y1', 'x2', 'y2']
     accuracy_df = []
     for image_id in df_gt['image_id'].unique():
         bboxes = df[df['image_id'] == image_id][cols].values
         gt_boxes = np.array(df_gt[df_gt['image_id'] == image_id][cols].values)
-        accuracy_df.append(accuracy(bboxes, gt_boxes, image_id=image_id, dist_thr=config.dist_thr))
+        accuracy_df.append(accuracy(bboxes, gt_boxes, image_id=image_id, dist_thr=dist_thr))
     stats = summarize_accuracy(accuracy_df)
     if return_full:
         return stats, pd.concat(accuracy_df, ignore_index=True)
