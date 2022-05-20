a3f54876d179a2e2e22456f6267cffb839a7fcec
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-05-20 13:46:17 -0500

    dataset class for mosaic detection

--- ml_utils/dataset/dataset_object_inference.py
+++ ml_utils/dataset/dataset_object_inference.py
@@ -47,3 +47,41 @@ class DatasetObjectInference(Dataset):
 
     def __len__(self) -> int:
         return self.image_ids.shape[0]
+
+
+class DatasetObjectInferenceMosaic(DatasetObjectInference):
+    """
+    Dataset class for prediction of bounding boxes in mosaic images.
+    """
+
+    def __init__(self, dataframe, image_dir, transforms=None, maxsize=None):
+        """
+        Initialize the Dataset instance.
+
+        Parameters
+        ----------
+        dataframe : pd.DataFrame
+            Dataframe with a list of image names
+        image_dir : str
+            Input directory with the images.
+        transforms : transform
+            Transform to apply before predictions.
+            Default is None
+        """
+        super().__init__(dataframe, image_dir, transforms, maxsize)
+
+    def __getitem__(self, index: int):
+        image_id = self.image_ids[index]
+
+        image = load_image(f'{self.image_dir}/{image_id}', self.maxsize)
+        x1, x2, y1, y2 = self.dataframe.iloc[index][['x1', 'x2', 'y1', 'y2']].values
+        image = image[y1:y2, x1:x2]
+
+        if self.transforms:
+            sample = {
+                'image': image,
+            }
+            sample = self.transforms(**sample)
+            image = sample['image']
+
+        return image, image_id, (y1, x1)
